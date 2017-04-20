548e6b178ff63ae9941f85eb16c0dea278011a45
BRIAN WONG <bhwong@10-150-45-27.dhcp-m.deakin.edu.au>
2017-04-20 10:11:37 +1000

    Week6 unfinished

---
+++ week6/week6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  week6
+//
+//  Created by BRIAN WONG on 20/04/2017.
+//  Copyright © 2017 BRIAN WONG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
